f0be6ab2e417a38c595fd436d90593fed59c3d32
Lars Andersen <la11@student.aau.dk>
2015-02-24 08:33:16 +0100

    initial commit

---
+++ app/src/main/java/dk/aau/cs/psylog/psylog_lightmodule/LightListener.java
@@ -0,0 +1,38 @@
+package dk.aau.cs.psylog.psylog_lightmodule;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+
+public class LightListener implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+
+    public LightListener(Context context) {
+        mSensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mSensorManager.registerListener(this,mSensor,2000000);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_LIGHT) {
+            float lx = sensorEvent.values[0];
+            Log.i("Light Readings", "Light:" + lx + " lx");
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    public void stopSensor() {
+        mSensorManager.unregisterListener(this);
+    }
+}
