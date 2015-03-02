32b02e8176bf9f2af2b8caf902fcafca2f27167b
Mathias Winde <Mapede11@student.aau.dk>
2015-03-02 08:52:31 +0100

    Benytter nu den opdatered version af module_lib

--- app/src/main/java/dk/aau/cs/psylog/psylog_lightmodule/LightSensorService.java
+++ app/src/main/java/dk/aau/cs/psylog/psylog_lightmodule/LightSensorService.java
@@ -1,16 +1,11 @@
 package dk.aau.cs.psylog.psylog_lightmodule;
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
 import dk.aau.cs.psylog.module_lib.SuperService;
 
 public class LightSensorService extends SuperService
 {
     @Override
-    public void onCreate()
-    {
-        super.onCreate();
+    public void setSensor() {
         sensor = new LightListener(this);
     }
 }
