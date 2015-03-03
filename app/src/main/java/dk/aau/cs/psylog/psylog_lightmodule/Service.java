5c0a428c9c9abff3f11937551a69cc1ed84b751c
Mathias Winde <Mapede11@student.aau.dk>
2015-03-03 09:39:41 +0100

    Ændret LightSensorService til Service

--- app/src/main/java/dk/aau/cs/psylog/psylog_lightmodule/Service.java
+++ app/src/main/java/dk/aau/cs/psylog/psylog_lightmodule/Service.java
@@ -2,7 +2,7 @@ package dk.aau.cs.psylog.psylog_lightmodule;
 
 import dk.aau.cs.psylog.module_lib.SuperService;
 
-public class LightSensorService extends SuperService
+public class Service extends SuperService
 {
     @Override
     public void setSensor() {
